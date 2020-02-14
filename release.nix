55cb2db0972b7f65ee7078d8bf4bc6f5fdf6824b
Marek Mahut <marek.mahut@gmail.com>
2020-02-14 19:55:34 +0100

    release.nix: init

---
+++ release.nix
@@ -0,0 +1,12 @@
+let
+  pkgs = import (builtins.fetchTarball {
+  	# nixpkgs 19.09 2020-01-24
+  	url = "https://github.com/NixOS/nixpkgs/archive/e6391b4389e10a52358bd94b3031238648818b0a.tar.gz";
+  	sha256 = "07nkaihks0k60krghd900x8zhfrsxmvdchlf5fzqmhgmfw5x6krv";
+	}) {};
+in
+{
+  trezord = pkgs.trezord.overrideAttrs (old: rec {
+    src = pkgs.lib.cleanSource ../.;
+  });
+}
